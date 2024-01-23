95447faef684ba3b634ad03fa939307b6cdea22f
Ahmed Kouki <koukiahmed23@gmail.com>
2024-01-23 20:05:15 +0100

    exam ahmed 3

--- src/Main.java
+++ src/Main.java
@@ -24,8 +24,5 @@ public class Main {
 
         personne.toString();
         etudiant1.toString();
-
-
-
         }
 }
--- src/enseignat.java
+++ src/enseignat.java
@@ -110,4 +110,6 @@ public class enseignat extends Personne {
         this.nationalite = nationalite;
     }
 
+
+
 }
